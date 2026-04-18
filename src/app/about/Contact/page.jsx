1a9440dfae9ebdda253524615b400a2647d9a202
Shipan Ahmed <shipanahmed1999@gmail.com>
2026-04-18 17:18:37 +0600

    added routing systems

---
+++ src/app/about/Contact/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactInfo = () => {
+    return (
+        <div>
+            <h2>this is contact informantion</h2>
+        </div>
+    );
+};
+
+export default ContactInfo;
